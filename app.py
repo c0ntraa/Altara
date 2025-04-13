47074a5fa0e192ea96cc0eebf1c32aae4fcaccef
Contra <aquastreak8@gmail.com>
2025-04-12 23:22:55 -0400

    Update app.py

--- app.py
+++ app.py
@@ -122,7 +122,7 @@ def summary_panel(info):
 # --- UI Input ---
 st.markdown("<div class='section'>", unsafe_allow_html=True)
 st.markdown("### 📈 Analyze a Stock")
-ticker = st.text_input("Enter Stock Symbol (ex. AAPL, TSLA)").upper()
+ticker = st.text_input("Enter Stock Symbol (ex. AAPL, TSLA, VOO)").upper()
 st.markdown("</div>", unsafe_allow_html=True)
 
 # --- Main Action ---
